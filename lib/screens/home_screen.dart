89e152d7e88584899e9d12487a121011394d2b61
ebtisam07 <439200954@student.ksu.edu.sa>
2023-06-12 21:37:29 +0300

    remove of arrow

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -112,6 +112,7 @@ class _HomeSceenState extends State<HomeSceen>
             height: MediaQuery.of(context).size.height * 0.1,
             width: MediaQuery.of(context).size.width * 0.1,
           ),
+          automaticallyImplyLeading: false,
         ),
         body: pages[index],
         bottomNavigationBar: NavigationBar(
