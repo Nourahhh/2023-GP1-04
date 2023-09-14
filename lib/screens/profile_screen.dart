5f616c75edeba311ead43c16744d9feef4289e20
ebtisam07 <439200954@student.ksu.edu.sa>
2023-09-14 15:50:40 +0300

    some edits

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -21,6 +21,7 @@ class _profilePageState extends State<profilePage> {
   bool changesMade = false; // Add a boolean variable to track changes
   bool isButtonEnabled = false; // Add a boolean variable to track button state
 
+
   @override
   void initState() {
     super.initState();
@@ -41,6 +42,7 @@ class _profilePageState extends State<profilePage> {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -54,221 +56,218 @@ class _profilePageState extends State<profilePage> {
         ),
       ),
       body: SafeArea(
-        child: Center(
-            child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'الاسم الأول',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                      ),
-                    ),
-                    SizedBox(
-                        height:
-                            5), // Adjusted the SizedBox height for better spacing
-
-                    Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.grey,
-                          width: 1.0,
-                        ),
-                        borderRadius: BorderRadius.circular(
-                            8.0), // Simplified BorderRadius
-                      ),
-                      child: ListTile(
-                        leading: Icon(Icons.person),
-                        title: TextFormField(
-                          initialValue:
-                              originalFirstName, // Use the original value
-                          onChanged: (newValue) {
-                            setState(() {
-                              originalFirstName =
-                                  newValue; // Update the original value locally
-                              changesMade =
-                                  true; // Set changesMade to true when changes are made
-                              isButtonEnabled = true;
-                            });
-                          },
-                          decoration: InputDecoration(
-                            border: InputBorder.none, // Hide the underline
-                          ),
-                          textDirection: TextDirection
-                              .rtl, // Force right-to-left direction
-                          textAlign:
-                              TextAlign.start, // Align to the start (left)
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-
-              SizedBox(height: 16),
-
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'الاسم الأخير',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                      ),
-                    ),
-                    SizedBox(
-                        height:
-                            5), // Adjusted the SizedBox height for better spacing
-
-                    Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.grey,
-                          width: 1.0,
-                        ),
-                        borderRadius: BorderRadius.circular(
-                            8.0), // Simplified BorderRadius
-                      ),
-                      child: ListTile(
-                        leading: Icon(Icons.person),
-                        title: TextFormField(
-                          initialValue:
-                              originalLastName, // Use the original value
-                          onChanged: (newValue) {
-                            setState(() {
-                              originalLastName =
-                                  newValue; // Update the original value locally
-                              changesMade =
-                                  true; // Set changesMade to true when changes are made
-                              isButtonEnabled = true;
-                            });
-                          },
-                          decoration: InputDecoration(
-                            border: InputBorder.none, // Hide the underline
-                          ),
-                          textDirection: TextDirection
-                              .rtl, // Force right-to-left direction
-                          textAlign:
-                              TextAlign.start, // Align to the start (left)
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-
-              SizedBox(height: 16),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('البريد الإلكتروني',
+        
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Center(
+              child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'الاسم الأول',
                         style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold)),
-                    SizedBox(height: 5),
-                    Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.grey,
-                          width: 1.0,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
                         ),
-                        borderRadius: BorderRadius.all(Radius.circular(8.0)),
                       ),
-                      child: ListTile(
-                        leading: Icon(Icons.email),
-                        title: Text(
-                          FirebaseService.email ?? "",
-                          style: TextStyle(
-                            color:
-                                Colors.grey[600], // Set the color to dark grey
+                      SizedBox(
+                          height:
+                              5), // Adjusted the SizedBox height for better spacing
+        
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.grey,
+                            width: 1.0,
+                          ),
+                          borderRadius: BorderRadius.circular(
+                              8.0), // Simplified BorderRadius
+                        ),
+                        child: ListTile(
+                          leading: Icon(Icons.person),
+                          title: TextFormField(
+                            initialValue:
+                                originalFirstName, // Use the original value
+                            onChanged: (newValue) {
+                              setState(() {
+                                originalFirstName =
+                                    newValue; // Update the original value locally
+                                changesMade =
+                                    true; // Set changesMade to true when changes are made
+                                isButtonEnabled = true;
+                              });
+                            },
+                            decoration: InputDecoration(
+                              border: InputBorder.none, // Hide the underline
+                            ),
+                            textDirection: TextDirection
+                                .rtl, // Force right-to-left direction
+                            textAlign:
+                                TextAlign.start, // Align to the start (left)
                           ),
                         ),
                       ),
-                    )
-                  ],
-                ),
-              ),
-              SizedBox(height: 30),
-              // Adjusted the SizedBox height for better spacing
-              ElevatedButton(
-                onPressed:
-                    isButtonEnabled // Enable the button based on the isButtonEnabled variable
-                        ? () {
-                            if (originalFirstName != null &&
-                                originalFirstName.isNotEmpty) {
-                              FirebaseService.first_name = originalFirstName;
-                              updateInfo('firstName', originalFirstName);
-                            }
-
-                            if (originalLastName != null &&
-                                originalLastName.isNotEmpty) {
-                              FirebaseService.last_name = originalLastName;
-                              updateInfo('lastName', originalLastName);
-                            }
-
-                            // After updating Firestore, show a success message
-                            showDialog(
-                              context: context,
-                              builder: (BuildContext context) {
-                                isButtonEnabled =
-                                    false; // Disable the button when the dialog is displayed
-                                return AlertDialog(
-                                  title: Text(
-                                      'تم الحفظ بنجاح'), // Success message in Arabic
-                                  content: Text(
-                                      'تم حفظ التغييرات بنجاح.'), // Success message in Arabic
-                                  actions: <Widget>[
-                                    TextButton(
-                                      onPressed: () {
-                                        Navigator.of(context)
-                                            .pop(); // Close the dialog
-                                        isButtonEnabled =
-                                            true; // Enable the button after closing the dialog
-                                      },
-                                      style: TextButton.styleFrom(
-                                        primary: Colors
-                                            .blue, // Set the text color of the OK button to blue
-                                      ),
-                                      child:
-                                          Text('حسنًا'), // OK button in Arabic
-                                    ),
-                                  ],
-                                );
-                              },
-                            );
-                          }
-                        : null, // Set onPressed to null to disable the button when it's not enabled
-                child: Text(
-                  'حفظ التغييرات', // Button text in Arabic
-                  style: TextStyle(
-                    color: isButtonEnabled
-                        ? Colors.white
-                        : Colors.grey[700], // Conditional text color
+                    ],
                   ),
                 ),
-                style: ElevatedButton.styleFrom(
-                  primary: Color.fromARGB(255, 43, 138, 159),
-                  // Set the background color of the button
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                    // Adjust the radius to match the container
+        
+                SizedBox(height: 16),
+        
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'الاسم الأخير',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      SizedBox(
+                          height:
+                              5), // Adjusted the SizedBox height for better spacing
+        
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.grey,
+                            width: 1.0,
+                          ),
+                          borderRadius: BorderRadius.circular(
+                              8.0), // Simplified BorderRadius
+                        ),
+                        child: ListTile(
+                          leading: Icon(Icons.person),
+                          title: TextFormField(
+                            initialValue:
+                                originalLastName, // Use the original value
+                            onChanged: (newValue) {
+                              setState(() {
+                                originalLastName =
+                                    newValue; // Update the original value locally
+                                changesMade =
+                                    true; // Set changesMade to true when changes are made
+                                isButtonEnabled = true;
+                              });
+                            },
+                            decoration: InputDecoration(
+                              border: InputBorder.none, // Hide the underline
+                            ),
+                            textDirection: TextDirection
+                                .rtl, // Force right-to-left direction
+                            textAlign:
+                                TextAlign.start, // Align to the start (left)
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ),
-              SizedBox(height: 30),
-            ],
-          ),
-        )),
+        
+                SizedBox(height: 16),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('البريد الإلكتروني',
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold)),
+                      SizedBox(height: 5),
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.grey,
+                            width: 1.0,
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                        ),
+                        child: ListTile(
+                          leading: Icon(Icons.email),
+                          title: Text(
+                            FirebaseService.email ?? "",
+                            style: TextStyle(
+                              color:
+                                  Colors.grey[600], // Set the color to dark grey
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(height: 30),
+                // Adjusted the SizedBox height for better spacing
+               ElevatedButton(
+  onPressed: isButtonEnabled
+      ? () {
+          if (originalFirstName != null &&
+              originalFirstName.isNotEmpty) {
+            FirebaseService.first_name = originalFirstName;
+            updateInfo('firstName', originalFirstName);
+          }
+
+          if (originalLastName != null &&
+              originalLastName.isNotEmpty) {
+            FirebaseService.last_name = originalLastName;
+            updateInfo('lastName', originalLastName);
+          }
+
+          // After updating Firestore, show a success message
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              isButtonEnabled = false;
+              FocusScope.of(context).unfocus(); // Hide the keyboard
+              return AlertDialog(
+                title: Text(
+                    'تم الحفظ بنجاح'),
+                content: Text(
+                    'تم حفظ التغييرات بنجاح.'),
+                actions: <Widget>[
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                      isButtonEnabled = true;
+                    },
+                    style: TextButton.styleFrom(
+                      primary: Colors.blue,
+                    ),
+                    child: Text('حسنًا'),
+                  ),
+                ],
+              );
+            },
+          );
+        }
+      : null,
+  child: Text(
+    'حفظ التغييرات',
+    style: TextStyle(
+      color: isButtonEnabled
+          ? Colors.white
+          : Colors.grey[700],
+    ),
+  ),
+  style: ElevatedButton.styleFrom(
+    primary: Color.fromARGB(255, 43, 138, 159),
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(8.0),
+    ),
+  ),
+),
+                SizedBox(height: 30),
+              ],
+            ),
+          )),
+        ),
       ),
     );
   }
