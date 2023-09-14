73b661b6cd5d21930963eef68f605bc4afb1557d
abeer439204515 <abeer.aj48@gmail.com>
2023-09-14 13:09:24 +0300

    edit auth for log in

--- lib/auth.dart
+++ lib/auth.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:naqi_app/screens/devices_screen.dart';
+import 'package:naqi_app/screens/home_screen.dart';
 import 'package:naqi_app/screens/login_screen.dart';
 
 class Auth extends StatelessWidget {
@@ -13,7 +14,7 @@ class Auth extends StatelessWidget {
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return DevicesPage();
+            return HomeSceen();
           } else {
             return LoginScreen();
           }
